627e3fde5db20ac60820bf7c6784964d621d3d2e
shoishizaki <shoishizaki1997@gmail.com>
2021-12-28 01:27:54 +0900

    Create Rock-paper-scissors app

--- Rock-paper-scissors/ContentView.swift
+++ Rock-paper-scissors/ContentView.swift
@@ -1,16 +1,84 @@
-//
-//  ContentView.swift
-//  Rock-paper-scissors
-//
-//  Created by 石崎將 on 2021/12/27.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var answerNumber = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+            
+            Spacer()
+            
+            if answerNumber == 0 {
+                
+                Text("これからじゃんけんをします")
+                    .padding(.bottom)
+                
+            } else if answerNumber == 1 {
+                
+                Image("gu")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Spacer()
+                
+                Text("グー")
+                    .padding(.bottom)
+                
+            } else if answerNumber == 2 {
+                
+                Image("choki")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Spacer()
+                
+                Text("チョキ")
+                    .padding(.bottom)
+                
+            } else {
+                
+                Image("pa")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Spacer()
+                
+                Text("パー")
+                    .padding(.bottom)
+                
+            }
+            
+            Button(action: {
+                
+                print("タップされたよ！")
+                
+                answerNumber = Int.random(in: 1...3)
+                
+                var newAnswerNumber = 0
+                
+                repeat {
+                    
+                    newAnswerNumber = Int.random(in: 1...3)
+                    
+                } while answerNumber == newAnswerNumber
+                
+                answerNumber = newAnswerNumber
+                
+            }) {
+                
+                Text("じゃんけんする!")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 100)
+                    .font(.title)
+                    .background(Color.pink)
+                    .foregroundColor(Color.white)
+                
+            }
+            
+        }
+
     }
 }
 
