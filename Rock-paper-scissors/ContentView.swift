841696dc2c18c2400ce1f31cce8b508a38eaee2c
shoishizaki <shoishizaki1997@gmail.com>
2021-12-27 21:27:12 +0900

    Initial Commit

---
+++ Rock-paper-scissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rock-paper-scissors
+//
+//  Created by 石崎將 on 2021/12/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rock-paper-scissors/Rock_paper_scissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_paper_scissorsApp.swift
+//  Rock-paper-scissors
+//
+//  Created by 石崎將 on 2021/12/27.
+//
+
+import SwiftUI
+
+@main
+struct Rock_paper_scissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
